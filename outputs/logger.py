c6907bc55373590b52f87f4d2111442d5dd3dc1e
earat <earat@leeds.ac.uk>
2016-06-29 12:05:04 +0100

    Introduced USB backup

--- outputs/logger.py
+++ outputs/logger.py
@@ -3,60 +3,88 @@ import time
 import os
 import subprocess
 import output
+import usbbackup
 
 class Logger(output.Output):
-	remoteData = ["remotedir","remoteuser","remotehostname"]
-	requiredData = ["localdir"]
-	optionalData = remoteData
-	def __init__(self,data):
+    usbData = ["usb"]
+    remoteData = ["remotedir","remoteuser","remotehostname"]
+    requiredData = ["localdir"]
+    optionalData = remoteData + usbData
+    def __init__(self,data):
 
-		self.ld = data["localdir"]
-		# Check if local logging directory exists
-		if not os.path.exists(self.ld):
-			os.makedirs(self.ld)
+        self.ld = data["localdir"]
+        # Check if local logging directory exists
+        if not os.path.exists(self.ld):
+            os.makedirs(self.ld)
+        # Check if remoteBackup needs to be enabled
+        if set(self.remoteData) <= set(data):
+            # Check if remoteData is subset of list of keys of data to ensure all remote settings are available
+            rd = data["remotedir"]
+            ru = data["remoteuser"]
+            rh = data["remotehostname"]
+            #Remote address
+            raddr = "%s@%s" % (ru, rh)
+            # Ensure that remote address exists
+            # Create remote directory if does not exist
+            mkdir_cmd = 'ssh %s "mkdir -p %s"' % (raddr,rd)
+            p = subprocess.Popen(mkdir_cmd, shell=True).wait()
+            self.rd = rd
+            self.raddr = raddr
+        else:
+            print("Warning!: The given remote settings are incomplete so no remote backup!")
+            self.rd = None
+            self.raddr = None
+        # Check if usb backup needs to be enabled
+        if set(self.usbData) <= set(data):
+            usbStatus = data["usb"]
+            if usbStatus == "on":
+                self.usbStatus = True
+            else:
+                print("Warning!: The USB backup is disabled!")
+                self.usbStatus = False
+        else:
+            print("Warning!: The USB backup is disabled!")
+            self.usbStatus = False
+        # Get datetime to init the variable
+        self.lastdatetime = time.strftime("%Y-%m-%d-%H", time.localtime())
 
-		if set(self.remoteData) <= set(data):
-			# Check if remoteData is subset of list of keys of data to ensure all remote settings are available
-			rd = data["remotedir"]
-			ru = data["remoteuser"]
-			rh = data["remotehostname"]
-			#Remote address
-			raddr = "%s@%s" % (ru, rh)
-			# Ensure that remote address exists
-			# Create remote directory if does not exist
-			mkdir_cmd = 'ssh %s "mkdir -p %s"' % (raddr,rd)
-			p = subprocess.Popen(mkdir_cmd, shell=True).wait()
-			self.rd = rd
-			self.raddr = raddr
-		else:
-			print("Warning!: The given remote settings are incomplete so no remote backup!")
-			self.rd = None
-			self.raddr = None
+    def remoteBackup(self,ld,rd,raddr):
+        # local dir, remote dir and remote address
+        # Backup data to server
 
-	def write(self,sensorName,data):
-		today = time.strftime("%Y-%m-%d-%H", time.localtime())
-		logfile = os.path.join(self.ld, today) +"-" + sensorName
-		with open(logfile, 'a') as f: # Open log file
-				f.write(data)
-				f.flush() # Properly write to disk
-	
-	def backup(self,ld,rd,raddr):
-		# local dir, remote dir and remote address
-		# Backup data to server
-		
-		# Here we format the remote location as 'username@hostname:'location'
-		remote = "%s:'%s'" % (raddr, rd)	
-		
-		# rsync
-		rsync_cmd = 'rsync -arz %s/ %s/' % (ld, remote)
-		p = subprocess.Popen(rsync_cmd, shell=True).wait()	
-		
-	def outputData(self,dataPoints):
-		"""
-		The data comes in as a dictionary
-		"""
-		data = dataPoints["value"]
-		sensorName = dataPoints["sensorName"]
-		self.write(sensorName,data)
-		if self.rd != None and self.raddr != None:
-		        self.backup(self.ld,self.rd,self.raddr)
+        # Here we format the remote location as 'username@hostname:'location'
+        remote = "%s:'%s'" % (raddr, rd)
+
+        # rsync
+        rsync_cmd = 'rsync -arz %s/ %s/' % (ld, remote)
+        p = subprocess.Popen(rsync_cmd, shell=True).wait()
+
+    def usbBackup(self,ld):
+        # Back up to USB
+        media = "/media"
+        status = usbbackup.main(ld,media)
+
+    def outputData(self,dataPoints):
+        """
+        The data comes in as a dictionary
+        """
+        # Write to disk
+        data = dataPoints["value"]
+        sensorName = dataPoints["sensorName"]
+        datetime = time.strftime("%Y-%m-%d-%H", time.localtime())
+        logfile = os.path.join(self.ld, datetime) + "-" + sensorName
+        with open(logfile, 'a') as f: # Open log file
+            f.write(data)
+            f.flush() # Properly write to disk
+
+        # Backup
+        usbStatus = False
+        if self.rd != None and self.raddr != None:
+            self.remoteBackup(self.ld,self.rd,self.raddr)
+        if self.usbStatus:
+            usbStatus = self.usbBackup(self.ld)
+        if usbStatus:
+            if self.lastdatetime != datetime:
+                cmd = "find %s ! -name '%s' -type f -exec rm -f {} +" % (self.ld,logfile)
+                p = subprocess.Popen(cmd, shell=True).wait()
+                self.lastdatetime = datetime
---
+++ outputs/usbbackup.py
@@ -0,0 +1,64 @@
+import os
+import subprocess
+
+def unmount(mediadir):
+    for name in os.listdir(mediadir):
+        full = os.path.join(mediadir, name)
+        if os.path.ismount(full):
+            print("Unmounting: " + full)
+            subprocess.Popen(["sudo","umount",full])
+
+def mounted(mediadir):
+    # Get a list of usb devices plugged in
+    deviceList = checkusb()
+    if not deviceList:
+        print("Nothing to see here")
+        unmount(mediadir)
+    else:
+        # Mount the first usb device if not mounted already
+        device = deviceList[0]
+        name = os.path.basename(device)
+        mountdir = os.path.join(mediadir,name)
+        if not os.path.exists(mountdir):
+            subprocess.Popen(["sudo","mkdir",mountdir])
+        if not os.path.ismount(mountdir):
+            print("Mounting: " + mountdir)
+            cmd = 'sudo mount -o rw,umash=000 %s %s' % (device,mountdir)
+            p = subprocess.Popen(cmd,shell=True).wait
+        return mountdir
+
+def checkusb():
+    idPath = '/dev/disk/by-id/'
+    deviceList = []
+    for name in os.listdir(idPath):
+        if "usb" in name:
+            if "part" in name:
+                full = os.path.join(idPath, name)
+                if os.path.islink(full):
+                    realpath = os.path.realpath(full)
+                    deviceList = deviceList + [realpath]
+    return deviceList
+
+def main(local,remote):
+    # Back up to USB
+    # Get mountpoint
+    mount = mounted(remote)
+    # If a USB is plugged in, then rsync the data to USB
+    if mount != None:
+        print("Backing up to USB")
+        rsync_cmd = 'rsync -arz %s/ %s/' % (local, mount)
+        p = subprocess.Popen(rsync_cmd, shell=True).wait()
+        if p == 0:
+            print("Backup Success")
+            return True
+        else:
+            print("Backup Failed")
+            return False
+    else:
+        print("No USB plugged in")
+        return False
+
+if __name__ == '__main__':
+    media = "/media"
+    ld = "~/logs"
+    main(ld,media)
