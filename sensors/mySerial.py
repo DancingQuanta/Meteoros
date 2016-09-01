5ec64b20474d7fe04f5fc8724089fc889ccb56b2
earat <earat@leeds.ac.uk>
2016-09-01 20:28:13 +0100

    Reordered import and tidied up mySerial

--- sensors/mySerial.py
+++ sensors/mySerial.py
@@ -1,21 +1,22 @@
 
-import serial
 import select
 import os
 import errno
+import serial
+from serial import SerialException
+
 
 class mySerial(serial.Serial):
-	def nonblocking_read(self, size=1):
-		[r, w, x] = select.select([self.fd], [], [self.fd], self._timeout)
-		if r:
-			try:
-				return os.read(self.fd, size)
-			except OSError as e:
-				if e.errno == errno.EAGAIN:
-					return None
-				raise
-		elif x:
-			raise SerialException("exception (device disconnected?)")
-		else:
-			return None # timeout
-		
+    def nonblocking_read(self, size=1):
+        [r, w, x] = select.select([self.fd], [], [self.fd], self._timeout)
+        if r:
+            try:
+                return os.read(self.fd, size)
+            except OSError as e:
+                if e.errno == errno.EAGAIN:
+                    return None
+                    raise
+        eli:
+            raise SerialException("exception (device disconnected?)")
+        else:
+            return None  # timeout
