bf11270ee0d1cbeac8fcd669a69053e47f8943ba
earat <earat@leeds.ac.uk>
2016-09-06 16:28:47 +0100

    Moved all code relating to plugsin into a function and changed imports to plugin classes

--- main.py
+++ main.py
@@ -13,8 +13,94 @@ import traceback
 import signal
 import configparser
 import inspect
-from sensors import sensor
-from outputs import output
+from sensors import Sensor
+from outputs import Output
+
+
+def get_subclasses(mod, cls):
+    for name, obj in inspect.getmembers(mod):
+        if hasattr(obj, "__bases__") and cls in obj.__bases__:
+            return obj
+
+
+def load_plugins(config, type):
+    # Load configuration from various plugsin of a type
+    if not os.path.isfile(config):
+        print("Unable to access config file: %s" % (config))
+        exit(1)
+
+    plugin_config = configparser.SafeConfigParser()
+    plugin_config.read(config)
+    names = plugin_config.sections()
+
+    plugins = []
+    for i in names:
+        try:
+            try:
+                filename = plugin_config.get(i, "filename")
+            except Exception:
+                msg = ("Error: no filename config option found for "
+                       "%s plugin %s" % (type.__name__, i))
+                print(msg)
+                raise
+            try:
+                enabled = plugin_config.getboolean(i, "enabled")
+            except Exception:
+                enabled = True
+
+            # If enabled, load the plugin
+            if enabled:
+                try:
+                    path = type.__dir__ + '.' + filename
+                    mod = __import__(path, fromlist=['a'])  # Why does this work?
+                except Exception:
+                    msg = ("Error: could not import %s module "
+                           "%s" % (type.__name__, filename))
+                    print(msg)
+                    raise
+
+                try:
+                    plugin_class = get_subclasses(mod, type)
+                    if plugin_class is None:
+                        raise AttributeError
+                except Exception:
+                    msg = ("Error: could not find a subclass of %s "
+                           "in module %s" % (type.__name__, filename))
+                    print(msg)
+                    raise
+
+                try:
+                    reqd = plugin_class.requiredData
+                except Exception:
+                    reqd = []
+                try:
+                    opt = plugin_class.optionalData
+                except Exception:
+                    opt = []
+
+                plugin_data = {}
+
+                class MissingField(Exception): pass
+
+                for requiredField in reqd:
+                    if plugin_config.has_option(i, requiredField):
+                        plugin_data[requiredField] = plugin_config.get(i, requiredField)
+                    else:
+                        msg = ("Error: Missing required field '%s' for %s"
+                               "plugin %s" % (requiredField, type.__name__, i))
+                        print(msg)
+                        raise MissingField
+                for optionalField in opt:
+                    if plugin_config.has_option(i, optionalField):
+                        plugin_data[optionalField] = plugin_config.get(i, optionalField)
+
+                instClass = plugin_class(plugin_data)
+                plugins.append(instClass)
+                print("Success: Loaded %s plugin %s" % (type.__name__, i))
+        except Exception as e:  # Add specific exception for missing module
+            print("Error: Did not import %s plugin %s" % (type.__name__, i))
+            raise e
+        return plugins
 
 
 class termColor(object):
@@ -183,147 +269,11 @@ if __name__ == "__main__":
     else:
         print("Unable to access config file: settings.cfg")
 
-    # Load configuration from various sensors
-    if not os.path.isfile('sensors.cfg'):
-        print("Unable to access config file: sensors.cfg")
-        exit(1)
+    # Load sensor plugins
+    sensorPlugins = load_plugins("sensors.cfg", Sensor)
 
-    # Load sensor configuration
-    def get_subclasses(mod, cls):
-        for name, obj in inspect.getmembers(mod):
-            if hasattr(obj, "__bases__") and cls in obj.__bases__:
-                return obj
-
-    sensorConfig = configparser.SafeConfigParser()
-    sensorConfig.read('sensors.cfg')
-    sensorNames = sensorConfig.sections()
-
-    sensorPlugins = []
-    for i in sensorNames:
-        try:
-            try:
-                filename = sensorConfig.get(i, "filename")
-            except Exception:
-                print("Error: no filename config option found for sensor plugin " + i)
-                raise
-            try:
-                enabled = sensorConfig.getboolean(i, "enabled")
-            except Exception:
-                enabled = True
-
-            #if enabled, load the plugin
-            if enabled:
-                try:
-                    mod = __import__('sensors.'+filename,fromlist=['a']) #Why does this work?
-                except Exception:
-                    print("Error: could not import sensor module " + filename)
-                    raise
-
-                try:
-                    sensorClass = get_subclasses(mod, sensor.Sensor)
-                    if sensorClass == None:
-                        raise AttributeError
-                except Exception:
-                    print("Error: could not find a subclass of sensor.Sensor in module " + filename)
-                    raise
-
-                try:
-                    reqd = sensorClass.requiredData
-                except Exception:
-                    reqd = []
-                try:
-                    opt = sensorClass.optionalData
-                except Exception:
-                    opt = []
-
-                pluginData = {}
-
-                class MissingField(Exception): pass
-
-                for requiredField in reqd:
-                    if sensorConfig.has_option(i,requiredField):
-                        pluginData[requiredField]=sensorConfig.get(i,requiredField)
-                    else:
-                        print("Error: Missing required field '" + requiredField + "' for sensor plugin " + i)
-                        raise MissingField
-                for optionalField in opt:
-                    if sensorConfig.has_option(i,optionalField):
-                        pluginData[optionalField]=sensorConfig.get(i,optionalField)
-                instClass = sensorClass(pluginData)
-                sensorPlugins.append(instClass)
-                print("Success: Loaded sensor plugin " + i)
-        except Exception as e: #add specific exception for missing module
-            print("Error: Did not import sensor plugin " + i )
-            raise e
-
-    if not os.path.isfile("outputs.cfg"):
-        print("Unable to access config file: outputs.cfg")
-    outputConfig = configparser.SafeConfigParser()
-    outputConfig.read("outputs.cfg")
-    outputNames = outputConfig.sections()
-    outputPlugins = []
-
-    for i in outputNames:
-        try:
-            try:
-                filename = outputConfig.get(i,"filename")
-            except Exception:
-                print("Error: no filename config option found for output plugin " + i)
-                raise
-            try:
-                enabled = outputConfig.getboolean(i,"enabled")
-            except Exception:
-                enabled = True
-
-            #if enabled, load the plugin
-            if enabled:
-                try:
-                    mod = __import__('outputs.'+filename,fromlist=['a']) #Why does this work?
-                except Exception:
-                    print("Error: could not import output module " + filename)
-                    raise
-
-                try:
-                    outputClass = get_subclasses(mod,output.Output)
-                    if outputClass == None:
-                        raise AttributeError
-                except Exception:
-                    print("Error: could not find a subclass of output.Output in module " + filename)
-                    raise
-                try:
-                    reqd = outputClass.requiredData
-                except Exception:
-                    reqd = []
-                try:
-                    opt = outputClass.optionalData
-                except Exception:
-                    opt = []
-
-                if outputConfig.has_option(i, "async"):
-                    async = outputConfig.getbool(i, "async")
-                else:
-                    async = False
-
-                pluginData = {}
-
-                class MissingField(Exception): pass
-
-                for requiredField in reqd:
-                    if outputConfig.has_option(i, requiredField):
-                        pluginData[requiredField]=outputConfig.get(i, requiredField)
-                    else:
-                        print("Error: Missing required field '" + requiredField + "' for output plugin " + i)
-                        raise MissingField
-                for optionalField in opt:
-                    if outputConfig.has_option(i, optionalField):
-                        pluginData[optionalField]=outputConfig.get(i, optionalField)
-                instClass = outputClass(pluginData)
-                instClass.async = async
-                outputPlugins.append(instClass)
-                print("Success: Loaded output plugin " + i)
-        except Exception as e:  # add specific exception for missing module
-            print("Error: Did not import output plugin " + i)
-            raise e
+    # Load output plugins
+    outputPlugins = load_plugins("outputs.cfg", Output)
 
     app = term(sensorPlugins,
                add_cr=args.crlf,
